87226c3343e7c4a79e9b6dc38a3d5e6a996f54aa
Veer Kumar <142009554+veerkumar01@users.noreply.github.com>
2026-05-29 14:25:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "20527556"))
-API_HASH = environ.get("API_HASH", "85d84f730c4e2f13bdf3efbce7d3352e")
+API_ID = int(environ.get("API_ID", "31685568"))
+API_HASH = environ.get("API_HASH", "436f53caee1dcae5eefcdf373716fccb")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8349653190"))
+OWNER = int(environ.get("OWNER", "2083529027"))
 CREDIT = environ.get("CREDIT", "TUC")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8349653190').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '2083529027').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8349653190').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '2083529027').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
